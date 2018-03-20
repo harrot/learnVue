beefbdc6bbc8aff9f6506530446b06d0af924b26
HUAN XU <harrot@gmail.com>
2018-03-19 22:42:07 -0400

    router example

---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div class="test2">
+        <span v-html="title"></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'test2',
+
+        data () {
+            return {
+               
+                title: '<h1>Hello World 2</h1>',
+             
+            }
+        },
+       
+    }
+</script>
+<!-- scoped limit CSS to this component only-->
+<style scoped>
+h1 {
+  font-weight: normal;
+  color: green;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue';
-import App from './App.vue'
+import VueRouter from 'vue-router'
+import firstComp from './components/FirstComponent'
+import secondComp from './components/SecondComponent'
+
+Vue.use(VueRouter)
+
+
+const router = new VueRouter({
+	mode: 'history',
+	base: __dirname,
+	routes: [
+		{path:'/', component: firstComp},
+		{path: '/test', component: secondComp}
+	]
+})
 
 new Vue({
-	el: '#app',
-	template: '<App/>',
-	components: { App }
-});
+	router,
+	template: `
+		<div id='app'>
+			<h1> Vue example for router</h1>
+			<ul>
+				<li><router-link to="/">firstComp</router-link></li>
+				<li><router-link to="/test">secondComp</router-link></li>
+			</ul>
+			<router-view></router-view>
+		</div>
+	`,
+}).$mount('#app');
